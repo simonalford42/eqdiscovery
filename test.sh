846730de4342b19f7df6ce08b3062a9b18496ab3
Simon Alford <simonalford42@gmail.com>
2023-04-25 01:09:00 -0400

    test script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # put run into file with current date/time in format MM-DD_HH:MM:SS.out
-python linear_learner.py -s drag3 > out/$(date +"%m-%d_%H:%M:%S").out
+python linear_learner.py > out/$(date +"%m-%d_%H:%M:%S").out
