41135c5a2c87a98d8062fb7ceb92dd3cdc090046
Simon Alford <simonalford42@gmail.com>
2023-10-03 15:37:01 -0400

    simplifying boids

--- boids.py
+++ boids.py
@@ -2,120 +2,108 @@ import numpy as np
 import utils
 from scipy.spatial.distance import cdist
 import pygame
-import random
 
 FPS = 30
-WIDTH = 900
-HEIGHT = 600
+WIDTH = 800
+HEIGHT = 500
+
 MAX_SPEED = 8
-FLEE_RADIUS = 43
-MAX_FLEE_FORCE = 22
+FLEE_RADIUS = 5
 ALIGN_RADIUS = 120
 COHESION_RADIUS = 400
 DT = 1/FPS
+WRAP = True
+CLIP_VEL = True
+
+
+def length(vec):
+    return np.linalg.norm(vec)
+
+
+def normalize(vec):
+    return vec / length(vec)
+
+
+def scale_to_length(vec, length):
+    return normalize(vec) * length
+
+
+def clip(vec, max):
+    if length(vec) > max:
+        return scale_to_length(vec, max)
+    else:
+        return vec
+
+
+def wrap(pos):
+    if pos[0] > WIDTH:
+        pos[0] = 0
+    elif pos[0] < 0:
+        pos[0] = WIDTH
+
+    if pos[1] > HEIGHT:
+        pos[1] = 0
+    elif pos[1] < 0:
+        pos[1] = HEIGHT
+
+    return pos
+
 
 def simulate_step(pos, vel):
     n = len(pos)
     x_t, v_t, a_t = [], [], []
+    dist = cdist(pos, pos)
+
+    def separation(i):
+        return -sum(dist[i,j] for j in range(n) if j != i and dist[i, j] < FLEE_RADIUS)
 
 
-    def length(vec):
-        return np.linalg.norm(vec)
+    def separation2(i):
+        total = 0
 
-    def normalize(vec):
-        return vec / length(vec)
+        for j in range(n):
+            if j == i:
+                continue
 
-    def scale_to_length(vec, length):
-        return normalize(vec) * length
-
-    def separation(i, j):
-        '''
-        Function for Rule 1 - Separation
-        Steer to avoid crowding local flockmates
-        '''
-
-        steer = np.zeros(2)
-        dist = pos[i] - pos[j]
-        desired = np.zeros(2)
-
-        if dist[0] != 0 and dist[0] != 0 :
-            if length(dist) < FLEE_RADIUS:
-                desired = scale_to_length(dist, MAX_SPEED)
+            if dist[i, j] < FLEE_RADIUS:
+                desired = scale_to_length(dist[i, j], MAX_SPEED)
             else:
                 desired = scale_to_length(vel[i], MAX_SPEED)
-        steer = desired - vel[i]
-        if length(steer) > MAX_FLEE_FORCE:
-            steer = scale_to_length(steer, MAX_FLEE_FORCE)
 
-        return steer
+            total += desired - vel[i]
+
+        return total
 
     def alignment(i):
-        '''
-        Function for Rule 2 - Alignment
-        Steer towards the average heading of local flockmates
-        '''
-
-        align = np.zeros(2)
-        desired = np.zeros(2)
-        for j in range(n):
-            if j != i:
-                if vel[j][0] != 0 and vel[j][1] != 0:
-                    if length(pos[i] - pos[j]) < ALIGN_RADIUS:
-                        desired += normalize(vel[j]) * MAX_SPEED
+        desired = sum(vel[j] for j in range(n) if j != i and dist[i, j] < ALIGN_RADIUS)
 
         align = desired - vel[i]
-        align =  align // n
-
-        if length(align) > MAX_SPEED:
-            align = scale_to_length(align, MAX_SPEED)
+        align = align // n
 
         return align
 
     def cohesion(i):
-        '''
-        Function for Rule 3 - Cohesion
-        Steer to move towards the average position (center of mass) of local flockmates
-        '''
-        cohes = np.zeros(2)
-        average_location = np.zeros(2)
-        for j in range(n):
-            if j != i:
-                dist = pos[i] - pos[j]
-                if length(dist) < COHESION_RADIUS:
-                    average_location += pos[j]
-
-        average_location = average_location/(n-1)
-        cohes = average_location - pos[i]
-        cohes = scale_to_length(cohes, MAX_SPEED)
+        average_location = (1/(n-1)) * sum(pos[j] for j in range(n) if j != i and dist[i, j] < COHESION_RADIUS)
+        # 1/100 coeff from website: https://vergenet.net/~conrad/boids/pseudocode.html
+        cohes = (1/100) * (average_location - pos[i])
         return cohes
 
 
     for i in range(n):
-        new_a = np.zeros(2)
+        a0 = separation(i)
+        a1 = alignment(i)
+        a2 = cohesion(i)
 
-        for j in range(n):
-            if j != i:
-                new_a += separation(i, j)
-
-        new_a += alignment(i)
-        new_a += cohesion(i)
-
+        new_a = a0 + a1 + a2
         new_v = vel[i] + new_a * DT
 
-        if len(new_v) > MAX_SPEED:
-            new_v = scale_to_length(new_v, MAX_SPEED)
+        if CLIP_VEL:
+            new_v = clip(new_v, MAX_SPEED)
 
         new_x = pos[i] + new_v
 
-        if new_x[0] > WIDTH:
-            new_x[0] = 0
-        elif new_x[0] < 0:
-            new_x[0] = WIDTH
-
-        if new_x[1] > HEIGHT:
-            new_x[1] = 0
-        elif new_x[1] < 0:
-            new_x[1] = HEIGHT
+        if WRAP:
+            new_x = wrap(new_x)
 
         x_t.append(new_x)
         v_t.append(new_v)
