19c8ab19aa2f148d91a68491ab53ae86876d03f5
Simon Alford <sca63@cornell.edu>
2024-01-05 11:54:25 -0500

    eval with position predicting nn not just acc

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# sbatch -J hp313 --partition ellis train.sh --lr 1e-3
-# sbatch -J hp353 --partition ellis train.sh --lr 5e-3
-# sbatch -J hp314 --partition gpu train.sh --lr 1e-4
-# sbatch -J hp354 --partition gpu train.sh --lr 5e-4
-
-sbatch -J lr5e-0 --partition gpu train.sh --lr 5e-0 --note lr5e-0
-sbatch -J lr1e-0 --partition gpu train.sh --lr 1e-0 --note lr1e-0
-sbatch -J lr5e-1 --partition gpu train.sh --lr 5e-1 --note lr5e-1
-sbatch -J lr1e-1 --partition gpu train.sh --lr 1e-1 --note lr1e-1
-sbatch -J lr5e-2 --partition gpu train.sh --lr 5e-2 --note lr5e-2
-sbatch -J lr1e-2 --partition gpu train.sh --lr 1e-2 --note lr1e-2
-
+sbatch -J acc1e-4 --partition ellis train.sh --lr 1e-4
+sbatch -J acc1e-3 --partition ellis train.sh --lr 1e-3
+sbatch -J acc1e-5 --partition ellis train.sh --lr 1e-5
