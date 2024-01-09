bb6234a7ac850b8a3ec4d55fa53eacd54031e8b8
Simon Alford <simonalford42@gmail.com>
2024-01-09 12:51:38 -0500

    save

--- eval.py
+++ eval.py
@@ -25,17 +25,20 @@ def predict(model, init_pos, info, t=1, model_predicts_acceleration=False):
     model = model.to(DEVICE)
 
     # should match process_data in train_locusts.py
-    def embed_timestep(arr):
-        N, D = arr.shape
-        data = np.zeros((N+2, D+3))
-        data[2:N+2, :D] = arr.detach().cpu().numpy()
+    def embed_timestep(arr, info):
+        T, N, D = arr.shape
+        assert_equal(T, 3)
+        data = np.zeros((N+2, 3*D+3))
+        data[2:N+2, :D] = arr[2]
+        data[2:N+2, D:2*D] = arr[1]
+        data[2:N+2, 2*D:3*D] = arr[0]
         data[0, :D] = info['posA']
         data[1, :D] = info['posB']
         data[0:, D] = 1
-        data[1, D+1] = info['isA_HQ']
-        data[1, D+1] = info['isB_HQ']
-        data[0, D+2] = info['radA']
-        data[1, D+2] = info['radB']
+        data[1, 3*D+1] = info['isA_HQ']
+        data[1, 3*D+1] = info['isB_HQ']
+        data[0, 3*D+2] = info['radA']
+        data[1, 3*D+2] = info['radB']
         assert_equal(data.shape, (N+2, IN_DIM))
         return torch.tensor(data, dtype=torch.float32)
 
@@ -46,13 +49,11 @@ def predict(model, init_pos, info, t=1, model_predicts_acceleration=False):
     x, v, a = list(x), list(v), list(a)
 
     for i in range(t):
-        inp = embed_timestep(x[-1]).to(DEVICE)
+        inp = embed_timestep(x[-3:], info).to(DEVICE)
         inp = rearrange(inp, 'n d -> () n d')
         inp = inp
         pred = model(inp)[0]
-        new_a = train_locusts.get_acceleration(pred)
-        new_v = v[-1] + locusts.DT * new_a
-        new_x = x[-1] + locusts.DT * new_v
+
         if model_predicts_acceleration:
             new_a = train_locusts.get_acceleration(pred)
             acc_scale = 1e4
@@ -62,7 +63,7 @@ def predict(model, init_pos, info, t=1, model_predicts_acceleration=False):
             a.append(new_a) # a_i
             v.append(new_v) # v_i+1 = v_i + a_i * dt
         else:
-            new_x = pred
+            new_x = train_locusts.get_position(pred)
 
         x.append(new_x) # x_i+2 = x_i+1 + v_i+1 * dt
 
@@ -81,29 +82,32 @@ def copy_ckpt(path):
     subprocess.run(['mv', path, '~/to_copy/model.ckpt'])
 
 
-
 # path = get_ckpt_path('simonalford42/locust_transformer/p01izwoi')
 # copy_ckpt(path)
 # assert False
 
-path = 'model.ckpt'
+path = 'model_pos.ckpt'
 model = Transformer.load_from_checkpoint(path)
+
 # validation
 # data, info = locusts.import_data('05UE20200708')
 # training
-data, info = locusts.import_data('05EQ20200207')
+data, info = locusts.import_data('01EQ20200706')
 
 data = torch.tensor(data, dtype=torch.float32)
 # data: [T, N, D]
 
-data = data[:10000]
-x, v, a = predict(model, data[:100], info, t=10000)
-print('actual data')
-locusts.visualize_data(data.detach().numpy(), info, speedup=25)
-print('predictions')
-locusts.visualize_data(x.detach().numpy(), info, speedup=25)
-
+T = 100
+data = data[:T+2]
 # x: [T+t, N, D]
+x = predict(model, data[:2], info, t=T)
+print('actual data')
+locusts.visualize_data(data.detach().numpy(), info, speedup=1)
+print('predictions')
+locusts.visualize_data(x.detach().numpy(), info, speedup=1)
+assert_equal(data.shape, x.shape)
+print(((data - x)**2).mean())
+
 
 # check that embed_timestep returns the same thing as process_data
 
--- train_locusts.py
+++ train_locusts.py
@@ -13,7 +13,7 @@ from utils import assert_equal
 import locusts
 import pickle
 
-IN_DIM = 5
+IN_DIM = 9
 OUT_DIM = 2
 
 class RFF(nn.Module):
@@ -49,8 +49,10 @@ class Transformer(L.LightningModule):
         self.nhead = args.nhead
         assert args.d_model % self.nhead == 0, "d_model must be divisible by nhead"
 
-        # self.embedding = nn.Linear(in_dim, args.d_model)
-        self.embedding = RFF(in_dim, args.d_model, args.rff_scale)
+        if self.args.embedding == 'proj':
+            self.embedding = nn.Linear(in_dim, args.d_model)
+        else:
+            self.embedding = RFF(in_dim, args.d_model, args.rff_scale)
         encoder_layer = nn.TransformerEncoderLayer(d_model=args.d_model, nhead=self.nhead, batch_first=True)
         self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=3)
         self.projection = nn.Linear(args.d_model, out_dim)
@@ -59,6 +61,7 @@ class Transformer(L.LightningModule):
         x = self.embedding(x)
         x = self.transformer(x)
         x = self.projection(x)
+        # return torch.zeros_like(x)
         return x
 
     def training_step(self, batch, batch_idx):
@@ -102,8 +105,8 @@ def get_train_val_filenames():
     # ]
 
     train_filenames = [
-        # '30EQ20191203'
-        # '05EQ20200813',
+        '30EQ20191203',
+        '05EQ20200813',
         '01EQ20200706',
     ]
     val_filenames = train_filenames
@@ -140,7 +143,18 @@ def get_acceleration(prediction):
     # the first two rows are the food particles
     return prediction[..., 2:N+2, :]
 
-def make_dataloaders(train_filenames, val_filenames):
+
+def get_position(prediction):
+    '''
+    input: [..., N+2, D]
+    output: [..., N, D]
+    '''
+    N = prediction.shape[-2] - 2
+    # the first two rows are the food particles
+    return prediction[..., 2:N+2, :]
+
+
+def make_dataloaders(train_filenames, val_filenames, predict_acceleration=False):
     print('Loading data...')
     train_data = {}
     val_data = {}
@@ -162,71 +176,89 @@ def make_dataloaders(train_filenames, val_filenames):
     # (2) if the food is high quality or not
     # (3) the radius of the food location
     # so the final dim is D + 3
+    # then we add the past 2 timesteps as well to enable seeing velocity and acc
+    # so the final dim is 3*D + 3
+    # because we need at least two time steps of history, the final shape is (T-2, N+2, 3*D+3)
+    # or (T-4, N+2, 3*D+3) if we're predicting acceleration
 
     def process_data(arr, info):
         T, N, D = arr.shape
         assert_equal(D, 2)
 
         # make data
-        data = np.zeros((T, max_n+2, D+3))
-        data[:, 2:N+2, :D] = arr
+        data = np.zeros((T-2, max_n+2, 3*D+3))
+        data[:, 2:N+2, :D] = arr[2:] # x_t
+        data[:, 2:N+2, D:2*D] = arr[1:-1] # x_t-1
+        data[:, 2:N+2, 2*D:3*D] = arr[:-2] # x_t-2
+
         data[:, 0, :D] = info['posA']
         data[:, 1, :D] = info['posB']
         data[:, 0:, D] = 1
-        data[:, 1, D+1] = info['isA_HQ']
-        data[:, 1, D+1] = info['isB_HQ']
-        data[:, 0, D+2] = info['radA']
-        data[:, 1, D+2] = info['radB']
+        data[:, 1, 3*D+1] = info['isA_HQ']
+        data[:, 1, 3*D+1] = info['isB_HQ']
+        data[:, 0, 3*D+2] = info['radA']
+        data[:, 1, 3*D+2] = info['radB']
 
-        # make acceleration targets
-        x = arr
-        v = (1/locusts.DT) * (arr[1:] - arr[:-1])
-        a = (1/locusts.DT) * (v[1:] - v[:-1])
-        # pad acceleration targets.
-        acc = np.zeros((T-2, max_n+2, D))
-        acc[:, 2:N+2, :] = a
-        # data and acc need to be the same length
-        data = data[:-2]
+        if predict_acceleration:
+            # x_t+1 = x_t + v_t * dt
+            # v_t+1 = v_t + a_t * dt
+            # a_t = (v_t+1 - v_t) / dt
+            # v_t = (x_t+1 - x_t) / dt
+            # so a_t is calculated from x_t, x_t+1, x_t+2
 
-        # instead, have the targets be the next timestep's position
-        # next_pos = np.zeros((T-1, max_n+2, D))
-        # next_pos[:, :N, :] = arr[1:]
-        # data = data[:-1]
+            # make acceleration targets
+            x = arr[2:] # x_t, is only (T-2, ) long
+            v = (1/locusts.DT) * (x[1:] - x[:-1])
+            # this gives a (T-4, N, D) array of targets
+            a = (1/locusts.DT) * (v[1:] - v[:-1])
+            # pad acceleration targets.
+            acc = np.zeros((T-4, max_n+2, D))
+            acc[:, 2:N+2, :] = a
+            # data and acc need to be the same length
+            data = data[:-2]
+            # scale the acceleration targets so that the prediction task is in a typical range
+            acc_scale = 1e4
+            acc = acc * acc_scale
 
-        # T, N, d
-        data = torch.tensor(data, dtype=torch.float32)
-        # next_pos = torch.tensor(next_pos, dtype=torch.float32)
-        acc = torch.tensor(acc, dtype=torch.float32)
+            data = torch.tensor(data, dtype=torch.float32)
+            acc = torch.tensor(acc, dtype=torch.float32)
+            assert_equal(data.shape, (T-4, max_n+2, IN_DIM))
+            assert_equal(acc.shape, (T-4, max_n+2, OUT_DIM))
+            return data, acc
 
+        else:
+            # instead, have the targets be the next timestep's position
+            next_pos = np.zeros((T-3, max_n+2, D))
+            next_pos[:, :N, :] = arr[3:]
+            data = data[:-1]
 
-        # scale the acceleration targets so that the prediction task is in a typical range
-        acc_scale = 1e4
-        acc = acc * acc_scale
+            data = torch.tensor(data, dtype=torch.float32)
+            next_pos = torch.tensor(next_pos, dtype=torch.float32)
+            assert_equal(data.shape, (T-3, max_n+2, IN_DIM))
+            assert_equal(next_pos.shape, (T-3, max_n+2, OUT_DIM))
+            return data, next_pos
 
-        assert_equal(data.shape, (T-1, max_n+2, IN_DIM))
-        # assert_equal(next_pos.shape, (T-1, max_n+2, OUT_DIM))
-        assert_equal(acc.shape, (T-2, max_n+2, OUT_DIM))
-
-        # return data, next_pos
-        return data, acc
-
-    # def embed_timestep(arr):
-        # N, D = arr.shape
-        # data = np.zeros((N+2, D+3))
-        # data[2:N+2, :D] = arr
-        # data[0, :D] = info['posA']
-        # data[1, :D] = info['posB']
-        # data[0:, D] = 1
-        # data[1, D+1] = info['isA_HQ']
-        # data[1, D+1] = info['isB_HQ']
-        # data[0, D+2] = info['radA']
-        # data[1, D+2] = info['radB']
-        # assert_equal(data.shape, (N+2, IN_DIM))
-        # return torch.tensor(data, dtype=torch.float32)
+    # should match process_data in train_locusts.py
+    # def embed_timestep(arr, info):
+    #     T, N, D = arr.shape
+    #     assert_equal(T, 3)
+    #     data = np.zeros((N+2, 3*D+3))
+    #     data[2:N+2, :D] = arr[2]
+    #     data[2:N+2, D:2*D] = arr[1]
+    #     data[2:N+2, 2*D:3*D] = arr[0]
+    #     data[0, :D] = info['posA']
+    #     data[1, :D] = info['posB']
+    #     data[0:, D] = 1
+    #     data[1, 3*D+1] = info['isA_HQ']
+    #     data[1, 3*D+1] = info['isB_HQ']
+    #     data[0, 3*D+2] = info['radA']
+    #     data[1, 3*D+2] = info['radB']
+    #     assert_equal(data.shape, (N+2, IN_DIM))
+    #     return torch.tensor(data, dtype=torch.float32)
 
     # check that embed_timestep returns the same thing as process_data
-    # for (arr, info) in data.values():
-        # assert_equal(embed_timestep(arr[0]), process_data(arr, info)[0][0][:info['n']+2])
+    # for (arr, info) in train_data.values():
+        # assert_equal(embed_timestep(arr[0:3], info), process_data(arr, info)[0][0][:info['n']+2])
     # assert False
 
     def make_dataset(data):
@@ -262,6 +294,9 @@ parser.add_argument("--note", type=str, default='')
 # parser.add_argument("--steps", type=float, default=None)
 parser.add_argument("--epochs", type=int, default=100)
 parser.add_argument("--rff_scale", type=float, default=1)
+parser.add_argument("--embedding", type=str, choices=['rff', 'proj'], default='rff')
+parser.add_argument("--predict_acc", action='store_true')
+
 # parser.add_argument("--val_check_interval", type=float, default=0.2)
 args = parser.parse_args()
 
@@ -273,7 +308,7 @@ if __name__ == '__main__':
     model = Transformer(IN_DIM, OUT_DIM, args)
 
     train_filenames, val_filenames = get_train_val_filenames()
-    train_dataloader, val_dataloader, dataset_info = make_dataloaders(train_filenames, val_filenames)
+    train_dataloader, val_dataloader, dataset_info = make_dataloaders(train_filenames, val_filenames, predict_acceleration=args.predict_acc)
     # save dataloaders to a pickle for faster loading later
     # with open(f'dataloaders.pkl', 'wb') as f:
         # pickle.dump((train_dataloader, val_dataloader, dataset_info), f)
